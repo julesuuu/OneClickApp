73fc1651f1d648a7389dc9828b96cc69bc974c52
Julius De Perio <julesrdeperio@gmail.com>
2026-02-24 15:03:04 +0800

    document controller for making a request

--- backend/src/controllers/documents.js
+++ backend/src/controllers/documents.js
@@ -1,8 +1,8 @@
 const documentsRouter = require("express").Router()
 const { request } = require("express")
 const Document = require("../models/Document")
-import { userExtractor } from "../utils/middleware"
-
+const User = require("../models/User")
+const { userExtractor } = require("../utils/middleware")
 documentsRouter.get("/", async (req, res) => {
   const documents = await Document.find({})
 
@@ -17,6 +17,51 @@ documentsRouter.get("/:id", async (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+documentsRouter.post("/", userExtractor, async (req, res) => {
+  const { credentialType, purpose, customPurpose, copies, notes } = req.body
+
+  if (!req.user) {
+    return res.status(401).json({ error: "User not authenticated or synced" })
+  }
+
+  const newDocument = new Document({
+    user: req.user._id,
+    credentialType,
+    purpose,
+    customPurpose: purpose === "Other" ? customPurpose : null,
+    copies: copies || 1,
+    notes,
+    status: "Pending",
+  })
+
+  try {
+    const savedDocument = await newDocument.save()
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: { documents: savedDocument._id },
+    })
+
+    const populateDoc = await savedDocument.populate("user", {
+      name: 1,
+      studentNumber: 1,
+    })
+
+    res.status(201).json(populateDoc)
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: "Failed to create request", details: error.message })
+  }
+})
+
+documentsRouter.get("/my-request", userExtractor, async (req, res) => {
+  const documents = await Document.find({ user: req.user._id }).sort({
+    createdAt: -1,
+  })
+
+  res.json(documents)
 })
 
 module.exports = documentsRouter
